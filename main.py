ad2fb6c765980d61c97e766d3009a626f0d723fa
Moses Avila <mosesavila9@gmail.com>
2026-04-29 18:06:19 -0700

    added logger to main

--- main.py
+++ main.py
@@ -31,3 +31,11 @@ DEFAULT_QUERIES = [
     "OpenAI",
 ]
 
+# logging 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
